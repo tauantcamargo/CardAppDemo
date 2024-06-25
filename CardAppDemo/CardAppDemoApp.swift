235da259b7a92f8821de48c7832d7d11a3ef89d0
Tauan Camargo <lukyhenson@gmail.com>
2024-06-24 22:10:18 -0300

    Initial Commit

---
+++ CardAppDemo/CardAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppDemoApp.swift
+//  CardAppDemo
+//
+//  Created by Tauan Tathiell Camargo on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CardAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
