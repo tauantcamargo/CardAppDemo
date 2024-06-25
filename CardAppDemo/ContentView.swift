a7956acd6098c974e20600e7ffa3a8aa302c558e
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 08:20:51 -0300

    feat: add card view

--- CardAppDemo/ContentView.swift
+++ CardAppDemo/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.customBlue)
+                .ignoresSafeArea()
+            
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("niagrafalls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack {
+                    Text("Niagra Falls")
+                        .font(.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                }
+                Text("Come visit the falls for an experience of a lifetime.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(
+                Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+                    
+            )
+            .padding()
         }
-        .padding()
     }
 }
 
